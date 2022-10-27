dd5cbbe82450d17a0b068fc9eaaa4c244d75ed7f
matte <matte@host.docker.internal>
2022-10-28 01:41:27 +0200

    first commit

---
+++ it.splitter.holidaymanager.presentation/src/main/java/it/splitter/holidaymanager/presentation/SplitterizerHolidaymanagerApplication.java
@@ -0,0 +1,13 @@
+package it.splitter.holidaymanager.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitterizerHolidaymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitterizerHolidaymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/spillter/splitterizer/holidaymanager/SplitterizerHolidaymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.spillter.splitterizer.holidaymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitterizerHolidaymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
